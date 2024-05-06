8c721f7dc815fddfb3db2e0d3a34f1e046d0253f
RELI <80000020+RELIHR@users.noreply.github.com>
2024-05-06 16:30:20 -0600

    ⛔🟠🟢 Added rotate status

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ def get_user_info(token):
     r = requests.get("https://discord.com/api/v10/users/@me", headers=header)
     if r.status_code == 200:
         user_info = r.json()
-        return user_info["username"] + "#" + user_info["discriminator"], True
+        return user_info["username"], True
     else:
         return "Invalid token", False
 
-def change_status(token, message, emoji_name, emoji_id):
+def change_status(token, message, emoji_name, emoji_id, status):
     header = {
         'authorization': token
     }
 
-    current_status = requests.get("https://discord.com/api/v8/users/@me/settings", headers=header).json()
+    current_status = requests.get("https://discord.com/api/v10/users/@me/settings", headers=header).json()
 
     custom_status = current_status.get("custom_status", {})
     if custom_status is None:
@@ -36,10 +36,11 @@ def change_status(token, message, emoji_name, emoji_id):
 
     jsonData = {
         "custom_status": custom_status,
-        "activities": current_status.get("activities", [])
+        "activities": current_status.get("activities", []),
+        "status": status
     }
 
-    r = requests.patch("https://discord.com/api/v8/users/@me/settings", headers=header, json=jsonData)
+    r = requests.patch("https://discord.com/api/v10/users/@me/settings", headers=header, json=jsonData)
     return r.status_code
 
 def clear_console():
@@ -58,39 +59,45 @@ config = load_config()
 token = config["token"]
 clear_enabled = config["clear_enabled"]
 clear_interval = config["clear_interval"]
-sleep_interval = config["sleep_interval"]
+speed_rotator = config["speed_rotator"]
+status_sequence = config["status_sequence"]
 
 status_count = 0  
 emoji_count = 0
 
 while True:
+    current_status = status_sequence[status_count % len(status_sequence)]
     user_info, is_valid_token = get_user_info(token)
     statuses = read_statuses("text.txt")
     emojis = read_statuses("emojis.txt")
-    for status in statuses:
-        time_formatted = color_text(time.strftime("%I:%M %p:"), Fore.MAGENTA)
-        if is_valid_token:
-            token_color_code = Fore.GREEN
-        else:
-            token_color_code = Fore.RED
-        token_masked = token[:10] + "*****"
-        token_info = f"{token_masked} | {user_info}"
-        token_colored = color_text(token_info, token_color_code)
-        status_colored = color_text(status, Fore.CYAN) 
 
-        emoji_data = emojis[emoji_count % len(emojis)].split(":")  
-        if len(emoji_data) == 2:
-            emoji_name, emoji_id = emoji_data
-        elif len(emoji_data) == 1:
-            emoji_name = emoji_data[0]
-            emoji_id = None
-        else:
-            print(f"Invalid emoji: {emojis[emoji_count % len(emojis)]}")
-            continue
-        print(f"{time_formatted} Status changed for: {token_colored}. New status: {status_colored}. | Emoji: {emoji_name}")
-        change_status(token, status, emoji_name, emoji_id)
-        status_count += 1
-        emoji_count += 1
-        time.sleep(sleep_interval)
-        if clear_enabled and status_count % clear_interval == 0:
-            clear_console()
+    status = statuses[status_count % len(statuses)]
+    time_formatted = color_text(time.strftime("%I:%M %p:"), Fore.MAGENTA)
+    if is_valid_token:
+        token_color_code = Fore.GREEN
+    else:
+        token_color_code = Fore.RED
+    token_masked = token[:6] + "******"
+    token_info = f"{token_masked} | {user_info}"
+    token_colored = color_text(token_info, token_color_code)
+    status_colored = color_text(status, Fore.CYAN)
+
+    emoji_data = emojis[emoji_count % len(emojis)].split(":")  
+    if len(emoji_data) == 2:
+        emoji_name, emoji_id = emoji_data
+    elif len(emoji_data) == 1:
+        emoji_name = emoji_data[0]
+        emoji_id = None
+    else:
+        print(f"Invalid emoji: {emojis[emoji_count % len(emojis)]}")
+        continue
+
+    print(f"{time_formatted} Status changed for: {token_colored}. New status message: {status_colored}. | Emoji: ({emoji_name}) | Status: {current_status}")
+    change_status(token, status, emoji_name, emoji_id, current_status)
+
+    status_count += 1
+    emoji_count += 1
+
+    time.sleep(speed_rotator)
+    if clear_enabled and status_count % clear_interval == 0:
+        clear_console()
